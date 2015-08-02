87497cf741188b7d5940c9b8dfc83ee9179fd75d
Serg Krivoblotsky <krivoblotsky@me.com>
2015-08-02 11:48:48 +0300

    Install script added

---
+++ install.sh
@@ -0,0 +1,6 @@
+for f in *.codesnippet
+do 
+   cp -v $f ~/Library/Developer/Xcode/UserData/CodeSnippets/${f%.codesnippet}.codesnippet
+done
+
+echo "Codesnippet has been installed. Restart XCode to use the snippet."
